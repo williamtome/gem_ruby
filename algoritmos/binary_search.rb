d2506fc9ce06c632b650d5ea48029c40dc473c62
William W. Tome <william.tomenh@gmail.com>
2021-04-21 12:03:09 -0300

    binary search finished

--- algoritmos/binary_search.rb
+++ algoritmos/binary_search.rb
@@ -1,20 +1,24 @@
 numbers = [2,5,8,12,16,23,38,56,72,91]
-number = 23
+number = 71
+
+def binary_search_recursive(arr_numbers, search_number, left, right)
+    if (left > right)
+        return false
+    end
+    
+    mid = (left + right) / 2;
+    
+    if (arr_numbers[mid] == search_number)
+        return true;
+    elsif (search_number < arr_numbers[mid])
+        return binary_search_recursive(arr_numbers, search_number, left, mid - 1);
+    else
+        return binary_search_recursive(arr_numbers, search_number, mid + 1, right);
+    end
+end
 
 def binary_search(arr_numbers, search_number)
-    size_array = arr_numbers.size
-    group_of_arrays = arr_numbers.each_slice((size_array / 2).to_i).to_a
-    
-    response = ''
-    group_of_arrays.each do |group|
-        if(group.include? search_number)
-            response = 'Found'
-            break
-        else
-            response = 'Not found'
-        end
-    end
-    return response
+    return binary_search_recursive(arr_numbers, search_number, 0, arr_numbers.length - 1)
 end
 
 puts binary_search(numbers, number)
