f09b3f87746d7b8db58ff107e9ab456455578cac
William Tomé <william.tomenh@gmail.com>
2021-04-24 22:17:52 -0300

    merge sort

---
+++ algoritmos/ordenacao/merge_sort.rb
@@ -0,0 +1,12 @@
+numeros = [4,7,2,6,4,1,8,2]
+
+def merge_sort(arr, inicio, fim)
+    if ((fim - inicio) > 1)
+        meio = (fim + inicio) / 2
+    end
+end
+
+inicio = numeros
+fim = numeros.size - 1
+
+merge_sort(numeros, inicio, fim)
