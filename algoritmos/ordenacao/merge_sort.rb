fbf9bbdc21a6a4405548aa8319c67c8619597607
William Tomé <william.tomenh@gmail.com>
2021-04-26 13:59:55 -0300

    wip - merge sort.

--- algoritmos/ordenacao/merge_sort.rb
+++ algoritmos/ordenacao/merge_sort.rb
@@ -1,12 +1,38 @@
 numeros = [4,7,2,6,4,1,8,2]
 
-def merge_sort(arr, inicio, fim)
-    if ((fim - inicio) > 1)
+def merge_sort(lista, inicio=0, fim=nil)
+    if fim == nil
+        fim = lista.size - 1
+    end
+
+    if (fim - inicio) > 1
         meio = (fim + inicio) / 2
+        merge_sort(lista, inicio, meio)
+        merge_sort(lista, meio, fim)
+        merge(lista, inicio, meio, fim)
     end
 end
 
-inicio = numeros
-fim = numeros.size - 1
+def merge(lista, inicio, meio, fim)
+    left = lista[inicio..meio]
+    right = lista[meio+1..fim]
+    top_left, top_right = 0,0
+    for k in inicio..fim
+        if top_left >= left.length
+            lista[k] = right[top_right]
+            top_right + 1
+        elsif top_right >= right.length
+            lista[k] = left[top_left]
+            top_left + 1
+        elsif left[top_left] < right[top_right]
+            lista[k] = left[top_left]
+            top_left + 1
+        else
+            lista[k] = right[top_right]
+            top_right + 1
+        end
+        k = k + 1
+    end
+end
 
-merge_sort(numeros, inicio, fim)
+puts merge_sort(numeros)
