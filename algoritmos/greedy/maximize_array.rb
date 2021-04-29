e1ec800919c454f70732c70ad6c708a88950385c
William Tomé <william.tomenh@gmail.com>
2021-04-29 09:42:02 -0300

    Trabalhando no algoritmo maximize array.

---
+++ algoritmos/greedy/maximize_array.rb
@@ -0,0 +1,38 @@
+def maximize_array(list, k)
+
+    # tamanho do array
+    n = list.length
+    i = 1
+    j = 0
+
+    while i <= k do
+        min = +2147483647
+        index = -1
+
+        while j < n-1 do
+            if list[j] < min
+                min = list[j]
+                index = j
+            end
+            puts list[j]
+            j += 1
+        end
+        
+        i += 1
+
+        if min == 0
+            break
+        end
+
+        list[index] = -list[index]
+    end
+    
+    sum = 0
+    for i in 0..n-1 do
+        sum += list[i]
+    end
+
+end
+
+arr = [-2, 0, 5, -1, 2]
+puts maximize_array(arr, 4).to_s
