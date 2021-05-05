fa5ccefbaead09605880514d2ca6ff91aa426973
William Tomé <william.tomenh@gmail.com>
2021-05-05 13:06:42 -0300

    feat: criado teste para a função merge sort.

--- lib/gem_ruby.rb
+++ lib/gem_ruby.rb
@@ -3,6 +3,7 @@
 require_relative "gem_ruby/algoritmos/busca/jump_search"
 require_relative "gem_ruby/algoritmos/busca/binary_search"
 require_relative "gem_ruby/algoritmos/busca/linear_search"
+require_relative "gem_ruby/algoritmos/ordenacao/merge_sort"
 
 module GemRuby
   class Error < StandardError; end
