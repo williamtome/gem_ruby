d4a95c8aa5560c4c69cdbdd768660f9f0032ee73
William Tomé <william.tomenh@gmail.com>
2021-05-05 07:59:41 -0300

    test: criado teste para o binary search.

--- lib/gem_ruby.rb
+++ lib/gem_ruby.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "gem_ruby/algoritmos/busca/jump_search"
+require_relative "gem_ruby/algoritmos/busca/binary_search"
 
 module GemRuby
   class Error < StandardError; end
