f992adfc9ca325e957bd3fa62ab8451244c2c06f
William Tomé <william.tomenh@gmail.com>
2021-05-05 07:59:07 -0300

    refactor: retornar o índice do número encontrado.

--- lib/gem_ruby/algoritmos/busca/binary_search.rb
+++ lib/gem_ruby/algoritmos/busca/binary_search.rb
@@ -1,15 +1,12 @@
-numbers = [2,5,8,12,16,23,38,56,72,91]
-number = 71
-
 def binary_search_recursive(arr_numbers, search_number, left, right)
     if (left > right)
-        return false
+        return -1
     end
     
     mid = (left + right) / 2;
     
     if (arr_numbers[mid] == search_number)
-        return true;
+        return mid;
     elsif (search_number < arr_numbers[mid])
         return binary_search_recursive(arr_numbers, search_number, left, mid - 1);
     else
@@ -21,4 +18,4 @@ def binary_search(arr_numbers, search_number)
     return binary_search_recursive(arr_numbers, search_number, 0, arr_numbers.length - 1)
 end
 
-puts binary_search(numbers, number)
+# binary_search([2,5,8,12,16,23,38,56,72,91], 72)
