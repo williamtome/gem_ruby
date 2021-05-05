de620a71657b00920130fe2f69d2a8237a14eae5
William Tomé <william.tomenh@gmail.com>
2021-05-05 08:12:26 -0300

    refactor: corrigido nome da função.

--- lib/gem_ruby/algoritmos/busca/linear_seach.rb
+++ lib/gem_ruby/algoritmos/busca/linear_seach.rb
@@ -1,4 +1,4 @@
-def linear_serch(arr_numbers, number)
+def linear_search(arr_numbers, number)
     arr_numbers.each_with_index do |item, index|
         if item === number
             return index
@@ -7,4 +7,4 @@ def linear_serch(arr_numbers, number)
     return -1
 end
 
-linear_serch([10,20,80,30,60,50,110,100,130,170], 110)
+linear_search([10,20,80,30,60,50,110,100,130,170], 110)
