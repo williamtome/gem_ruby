b2886d674413b221dfdb462960baff458a3c382b
William Tomé <william.tomenh@gmail.com>
2021-05-05 21:14:57 -0300

    test: criado teste para selection sort.

--- lib/gem_ruby.rb
+++ lib/gem_ruby.rb
@@ -6,6 +6,7 @@ require_relative "gem_ruby/algoritmos/busca/linear_search"
 require_relative "gem_ruby/algoritmos/ordenacao/merge_sort"
 require_relative "gem_ruby/algoritmos/ordenacao/counting_sort"
 require_relative "gem_ruby/algoritmos/ordenacao/bubble_sort"
+require_relative "gem_ruby/algoritmos/ordenacao/selection_sort"
 require_relative "gem_ruby/algoritmos/greedy/maximize_array"
 
 module GemRuby
