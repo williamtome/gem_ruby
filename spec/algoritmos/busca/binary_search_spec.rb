3c24e171676c3b848059e0b85defc26552174e83
William Tomé <william.tomenh@gmail.com>
2021-05-10 21:28:29 -0300

    refactor: melhorando os testes.

--- spec/algoritmos/busca/binary_search_spec.rb
+++ spec/algoritmos/busca/binary_search_spec.rb
@@ -1,6 +1,16 @@
-RSpec.describe "binary_search" do
-    it { expect(binary_search([2,5,8,12,16,23,38,56,72,91], 71)).to eq -1 }
-    it { expect(binary_search([2,5,8,12,16,23,38,56,72,91], 8)).to eq 2 }
-    it { expect(binary_search([7, 28, 5, 14, 20, 21], 21)).to eq 5 }
-    it { expect(binary_search([7, 28, 5, 14, 20, 21], 79)).to eq -1 }
+RSpec.describe Algoritmos::Busca do
+  describe ".linear_search" do
+    it "-1" do
+      arr = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
+      number = 71
+
+      result = Busca::BinarySearch.new.run(arr, number)
+
+      expect(result).to eq(-1)
+    end
+  end
+
+  # it { expect(binary_search([2,5,8,12,16,23,38,56,72,91], 8)).to eq 2 }
+  # it { expect(binary_search([7, 28, 5, 14, 20, 21], 21)).to eq 5 }
+  # it { expect(binary_search([7, 28, 5, 14, 20, 21], 79)).to eq -1 }
 end
