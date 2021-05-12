807346971fdf77f820e346145e68899fea03911f
William Tomé <william.tomenh@gmail.com>
2021-05-12 07:51:24 -0300

    refactor: added length of array on function parameter.

--- lib/gem_ruby/algoritmos/busca/jump_search.rb
+++ lib/gem_ruby/algoritmos/busca/jump_search.rb
@@ -1,28 +1,25 @@
-def jump(arr, x)
-    size_arr = arr.length
-    step = Math.sqrt(size_arr)
-    prev = 0
-    
-    while arr[[step, size_arr].min.to_i - 1] < x
+# frozen_string_literal: true
+
+module Algoritmos
+  class JumpSearch
+    def run(arr, x, size)
+      step = Math.sqrt(size)
+      prev = 0
+
+      while arr[[step, size].min.to_i - 1] < x
         prev = step
-        step += Math.sqrt(size_arr)
-        return -1 if prev >= size_arr
-    end
+        step += Math.sqrt(size)
+        return -1 if prev >= size
+      end
 
-    while arr[prev.to_i] < x
+      while arr[prev.to_i] < x
         prev += 1
-        
-        return -1 if prev == [step, size_arr].min
+        return -1 if prev == [step, size].min
+      end
+
+      return prev.to_i if arr[prev.to_i] == x
+
+      -1
     end
-
-    return prev.to_i if arr[prev.to_i] == x
-
-    return -1
-    
+  end
 end
-
-puts jump([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610], 55) # Saída: 10
-# puts jump([10, 20, 30, 50, 60, 80, 100, 110, 130, 170], 110) # Output: 7
-# puts jump([10, 20, 30, 50, 60, 80, 100, 110, 130, 170], 175) # Output: -1
-# puts jump([10, 20, 30, 50, 60, 80, 100, 110, 130, 170], 170) # Output: 9
-# puts jump([10, 20, 30, 50, 60, 80, 100, 110, 130, 170], 10) # Output: 0
