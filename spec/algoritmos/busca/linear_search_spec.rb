20279b2da0fea1823b7193aa924f070088d1b978
William Tomé <william.tomenh@gmail.com>
2021-05-12 07:16:11 -0300

    test: refactor the test cases.

--- spec/algoritmos/busca/linear_search_spec.rb
+++ spec/algoritmos/busca/linear_search_spec.rb
@@ -1,6 +1,10 @@
-RSpec.describe "linear_search" do
-    it { expect(linear_search([10,20,80,30,60,50,110,100,130,170], 110)).to eq 6 }
-    it { expect(linear_search([10,20,80,30,60,50,110,100,130,170], 171)).to eq -1 }
-    it { expect(linear_search([20, 4, 80, 35, 30, 19, 60, 50], 19)).to eq 5 }
-    it { expect(linear_search([20, 4, 80, 35, 30, 19, 60, 50], 0)).to eq -1 }
+# frozen_string_literal: true
+
+RSpec.describe Algoritmos do
+  describe ".linear_search" do
+    it { expect(Algoritmos::LinearSearch.new.run([10, 20, 80, 30, 60, 50, 110, 100, 130, 170], 110)).to eq 6 }
+    it { expect(Algoritmos::LinearSearch.new.run([10, 20, 80, 30, 60, 50, 110, 100, 130, 170], 171)).to eq(-1) }
+    it { expect(Algoritmos::LinearSearch.new.run([20, 4, 80, 35, 30, 19, 60, 50], 19)).to eq 5 }
+    it { expect(Algoritmos::LinearSearch.new.run([20, 4, 80, 35, 30, 19, 60, 50], 0)).to eq(-1) }
+  end
 end
