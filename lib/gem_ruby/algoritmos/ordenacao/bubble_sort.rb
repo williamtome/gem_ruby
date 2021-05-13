eff4e39aa3a25208bcb080605fe80a6d76e7243f
William Tomé <william.tomenh@gmail.com>
2021-05-13 08:19:53 -0300

    refactor: added function into module and class.

--- lib/gem_ruby/algoritmos/ordenacao/bubble_sort.rb
+++ lib/gem_ruby/algoritmos/ordenacao/bubble_sort.rb
@@ -1,24 +1,28 @@
-def bubble_sort(array)
-    last = array.length - 1
-  
-    loop do
-        i = 0
-        step = 0
-  
-        while i < last
+# frozen_string_literal: true
+
+module Algoritmos
+  module Ordenacao
+    class BubbleSort
+      def run(array)
+        last = array.length - 1
+        loop do
+          i = 0
+          step = 0
+
+          while i < last
             if array[i] > array[i + 1]
-                array[i], array[i + 1] = array[i + 1], array[i]
-                step += 1
+              array[i], array[i + 1] = array[i + 1], array[i]
+              step += 1
             end
             i += 1
-        end
-  
-        last -= 1
-  
-        return array if step.zero?
-    end
-  
-    array
-end
+          end
 
-puts bubble_sort([64, 34, 25, 12, 22, 11, 90]).to_s # [11, 12, 22, 25, 34, 64, 90]
+          last -= 1
+          return array if step.zero?
+        end
+
+        array
+      end
+    end
+  end
+end
