cbc9a234651b30f67a22eb87a5521b3ba5a37704
William Tomé <william.tomenh@gmail.com>
2021-05-13 08:28:41 -0300

    refactor: added class into module Busca.

--- lib/gem_ruby/algoritmos/busca/jump_search.rb
+++ lib/gem_ruby/algoritmos/busca/jump_search.rb
@@ -1,25 +1,27 @@
 # frozen_string_literal: true
 
 module Algoritmos
-  class JumpSearch
-    def run(arr, x, size)
-      step = Math.sqrt(size)
-      prev = 0
+  module Busca
+    class JumpSearch
+      def run(arr, x, size)
+        step = Math.sqrt(size)
+        prev = 0
 
-      while arr[[step, size].min.to_i - 1] < x
-        prev = step
-        step += Math.sqrt(size)
-        return -1 if prev >= size
+        while arr[[step, size].min.to_i - 1] < x
+          prev = step
+          step += Math.sqrt(size)
+          return -1 if prev >= size
+        end
+
+        while arr[prev.to_i] < x
+          prev += 1
+          return -1 if prev == [step, size].min
+        end
+
+        return prev.to_i if arr[prev.to_i] == x
+
+        -1
       end
-
-      while arr[prev.to_i] < x
-        prev += 1
-        return -1 if prev == [step, size].min
-      end
-
-      return prev.to_i if arr[prev.to_i] == x
-
-      -1
     end
   end
 end
