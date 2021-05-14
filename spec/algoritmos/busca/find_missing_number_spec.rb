1f78f396095278539ef68d2470344d6d4541009e
William Tomé <william.tomenh@gmail.com>
2021-05-13 22:16:11 -0300

    test: refactor the test cases.

--- spec/algoritmos/busca/find_missing_number_spec.rb
+++ spec/algoritmos/busca/find_missing_number_spec.rb
@@ -1,10 +1,23 @@
 # frozen_string_literal: true
 
-RSpec.describe Algoritmos do
-  describe ".find_missing_number" do
-    it { expect(Algoritmos::FindMissingNumber.new.run([1, 2, 3, 5, 4, 7])).to eq 6 }
-    it { expect(Algoritmos::FindMissingNumber.new.run([1, 2, 4, 6, 3, 7])).to eq 5 }
-    it { expect(Algoritmos::FindMissingNumber.new.run([3, 2, 4, 6, 1, 5, 9, 8])).to eq 7 }
-    it { expect(Algoritmos::FindMissingNumber.new.run([9, 8, 7, 5, 4, 3, 2, 1])).to eq 6 }
+RSpec.describe "Find Missing Number" do
+  it "is expect to eq 6" do
+    expect(Algoritmos::Busca::FindMissingNumber.new.run([1, 2, 3, 5, 4, 7])).to eq 6
+  end
+
+  it "is expect to eq 5" do
+    expect(Algoritmos::Busca::FindMissingNumber.new.run([1, 2, 4, 6, 3, 7])).to eq 5
+  end
+
+  it "is expect to eq 7" do
+    expect(Algoritmos::Busca::FindMissingNumber.new.run([3, 2, 4, 6, 1, 5, 9, 8])).to eq 7
+  end
+
+  it "is expect to eq 6" do
+    expect(Algoritmos::Busca::FindMissingNumber.new.run([9, 8, 7, 5, 4, 3, 2, 1])).to eq 6
+  end
+
+  it "is expect to eq 10" do
+    expect(Algoritmos::Busca::FindMissingNumber.new.run([9, 8, 7, 6, 5, 4, 3, 2, 1])).to eq 10
   end
 end
