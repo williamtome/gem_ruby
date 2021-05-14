c1e4a63b88e8f9b0619e34d727392457e3932b4a
William Tomé <william.tomenh@gmail.com>
2021-05-13 22:21:47 -0300

    test: refactor the test cases.

--- spec/algoritmos/busca/linear_search_spec.rb
+++ spec/algoritmos/busca/linear_search_spec.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
-RSpec.describe Algoritmos do
-  describe ".linear_search" do
-    it { expect(Algoritmos::LinearSearch.new.run([10, 20, 80, 30, 60, 50, 110, 100, 130, 170], 110)).to eq 6 }
-    it { expect(Algoritmos::LinearSearch.new.run([10, 20, 80, 30, 60, 50, 110, 100, 130, 170], 171)).to eq(-1) }
-    it { expect(Algoritmos::LinearSearch.new.run([20, 4, 80, 35, 30, 19, 60, 50], 19)).to eq 5 }
-    it { expect(Algoritmos::LinearSearch.new.run([20, 4, 80, 35, 30, 19, 60, 50], 0)).to eq(-1) }
-  end
+RSpec.describe "Linear Search" do
+  it { expect(Algoritmos::Busca::LinearSearch.new.run([10, 20, 80, 30, 60, 50, 110, 100, 130, 170], 110)).to eq 6 }
+  it { expect(Algoritmos::Busca::LinearSearch.new.run([10, 20, 80, 30, 60, 50, 110, 100, 130, 170], 171)).to eq(-1) }
+  it { expect(Algoritmos::Busca::LinearSearch.new.run([20, 4, 80, 35, 30, 19, 60, 50], 19)).to eq 5 }
+  it { expect(Algoritmos::Busca::LinearSearch.new.run([20, 4, 80, 35, 30, 19, 60, 50], 0)).to eq(-1) }
 end
