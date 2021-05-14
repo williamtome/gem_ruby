7be3ba8e28a8b367fc2d2f3cc33efd101baf6836
William Tomé <william.tomenh@gmail.com>
2021-05-14 06:52:02 -0300

    test: refactor the test cases.

--- spec/algoritmos/ordenacao/selection_sort_spec.rb
+++ spec/algoritmos/ordenacao/selection_sort_spec.rb
@@ -1,6 +1,8 @@
-RSpec.describe "selection_sort" do
-    it { expect(selection_sort([64, 25, 12, 22, 11])).to eq [11, 12, 22, 25, 64] }
-    it { expect(selection_sort([5, 1, 3, 8, 6])).to eq [1, 3, 5, 6, 8] }
-    it { expect(selection_sort([30, 84, 93, 40, 29, 80, 47, 6, 42, 83])).to eq [6, 29, 30, 40, 42, 47, 80, 83, 84, 93] }
-    it { expect(selection_sort([61, 15, 18, 46, 16, 28, 5, 39, 32, 58])).to eq [5, 15, 16, 18, 28, 32, 39, 46, 58, 61] }
+# frozen_string_literal: true
+
+RSpec.describe "Selection Sort" do
+  it { expect(Algoritmos::Ordenacao::SelectionSort.new.run([64, 25, 12, 22, 11])).to eq [11, 12, 22, 25, 64] }
+  it { expect(Algoritmos::Ordenacao::SelectionSort.new.run([5, 1, 3, 8, 6])).to eq [1, 3, 5, 6, 8] }
+  it { expect(Algoritmos::Ordenacao::SelectionSort.new.run([30, 84, 93, 40, 29, 80, 47, 6, 42, 83])).to eq [6, 29, 30, 40, 42, 47, 80, 83, 84, 93] }
+  it { expect(Algoritmos::Ordenacao::SelectionSort.new.run([61, 15, 18, 46, 16, 28, 5, 39, 32, 58])).to eq [5, 15, 16, 18, 28, 32, 39, 46, 58, 61] }
 end
