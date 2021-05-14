8c41d48ee314230bde12c4fcd5b7c2da4a7d74c9
William Tomé <william.tomenh@gmail.com>
2021-05-13 22:21:19 -0300

    refactor: added class into module Busca.

--- lib/gem_ruby/algoritmos/busca/linear_search.rb
+++ lib/gem_ruby/algoritmos/busca/linear_search.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 module Algoritmos
-  class LinearSearch
-    def run(arr_numbers, number)
-      arr_numbers.each_with_index { |item, index| return index if item == number }
-      -1
+  module Busca
+    class LinearSearch
+      def run(arr_numbers, number)
+        arr_numbers.each_with_index { |item, index| return index if item == number }
+        -1
+      end
     end
   end
 end
