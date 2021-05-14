b24f5e1628ae68bf068cdd2e5153379124aa881b
William Tomé <william.tomenh@gmail.com>
2021-05-13 22:15:54 -0300

    refactor: added class into module Busca.

--- lib/gem_ruby/algoritmos/busca/find_missing_number.rb
+++ lib/gem_ruby/algoritmos/busca/find_missing_number.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 module Algoritmos
-  class FindMissingNumber
-    def run(array)
-      max_num = array.size + 1
-      range_sum = max_num * (max_num + 1) / 2
-      range_sum - array.sum
+  module Busca
+    class FindMissingNumber
+      def run(array)
+        max_num = array.size + 1
+        range_sum = max_num * (max_num + 1) / 2
+        range_sum - array.sum
+      end
     end
   end
 end
